d5fd5250d84e00b8ed03cfcc18e4f0f1b5349feb
cobbiecobbie <Benjamin.Coban@gmail.com>
2023-03-23 15:45:50 +0100

    - Collectable prefab - Collectable.cs - - attached to every Collectable GO - CollectableManager.cs - - attached to Creeper

---
+++ Assets/Collectible/Collectable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        CollectableManager manager = other.GetComponent<CollectableManager>();
+        if (manager != null)
+        {
+            manager.CollectableCollected();
+            Debug.Log("Amount of Collectables: " + manager.CollectableAmount);
+            gameObject.SetActive(false);
+        }
+    }
+}
