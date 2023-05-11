532b98b29b3797cd5b5a9a5f397c93739e84b5ac
locojoetive <johannes.mic@gmail.com>
2023-05-11 08:12:42 +0200

    fix: however that could have happened

--- Assets/_Scripts/RMath.cs
+++ Assets/_Scripts/RMath.cs
@@ -42,7 +42,7 @@ namespace RootMath
 
     public static class RMath
     {
-        public static bool AreDirectíonsConsideredEqual(Vector3 _direction1, Vector3 _directions)
+        public static bool AreDirectionsConsideredEqual(Vector3 _direction1, Vector3 _directions)
         {
             return Vector3.Dot(_direction1, _directions) > 0.99f;
         }
