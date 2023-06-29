3ab5d2087656f2efc6cc4fe5d81629f0ccefee5a
locojoetive <johannes.mic@gmail.com>
2023-06-29 10:50:29 +0200

    feat: implement title screen and lloading first level

---
+++ Assets/_Game/Entities/Game/Game.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using DarkTonic.MasterAudio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Roots
+{
+    public class Game : MonoBehaviour
+    {
+        private static Game _instance;
+        public static Game Instance => _instance;
+        public UI ui;
+        
+        
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            OnLoadTitle();
+        }
+
+        public void OnLoadTitle()
+        {
+            SceneManager.LoadSceneAsync(1);
+        }
+
+        public void OnLoadFirstLevel()
+        {
+            StartCoroutine(
+                ui.FadeOut(
+                    () => SceneManager.LoadSceneAsync(2)
+                )
+            );
+        }
+        
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+ 
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            StartCoroutine(ui.FadeIn(null));
+        }
+
+    }
+}
+    
---
+++ Assets/_Game/Entities/Title/Title.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Roots
+{
+    
+    public class Title : MonoBehaviour
+    {
+        public Animator[] animators;
+        private Coroutine coroutine;
+        public void OnClick()
+        {
+            if (coroutine != null) return;
+            
+            foreach (var animator in animators)
+            {
+                animator.enabled = true;
+            }
+
+            coroutine = StartCoroutine(LoadLevelAfterAnimation());
+        }
+        
+        private IEnumerator LoadLevelAfterAnimation()
+        {
+            yield return new WaitForSeconds(2.0f);
+            FindObjectOfType<Game>().OnLoadFirstLevel();
+        }
+    }
+}
