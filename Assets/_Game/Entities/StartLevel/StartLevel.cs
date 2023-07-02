db011e3f6725e423d027908b989e87594ddd3895
locojoetive <johannes.mic@gmail.com>
2023-07-03 00:37:07 +0200

    refactor: remove unused member and import

--- Assets/_Game/Entities/StartLevel/StartLevel.cs
+++ Assets/_Game/Entities/StartLevel/StartLevel.cs
@@ -1,12 +1,10 @@
 using Cinemachine;
 using Creeper;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class StartLevel : MonoBehaviour
 {
     private InputController _inputController;
-    public CinemachineVirtualCamera camera;
 
     private void Awake()
     {
