04ad0e7be1a3f0ad04d452dbc9346f9bd47e7ff1
locojoetive <johannes.mic@gmail.com>
2023-07-02 17:52:37 +0200

    feat: add interaction for satellite and lamp

---
+++ Assets/_Game/Entities/House/OnTouchSetDynamic.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using RootMath;
+using UnityEngine;
+
+public class OnTouchSetDynamic : MonoBehaviour
+{
+       private Rigidbody targetRigidbody;
+
+       private void Awake()
+       {
+              targetRigidbody = GetComponent<Rigidbody>();
+              targetRigidbody.isKinematic = true;
+       }
+
+       private void OnCollisionEnter(Collision other)
+       {
+              if (RMath.IsLayerPlayerPhysicsCollider(other.gameObject.layer))
+              {
+                     targetRigidbody = GetComponent<Rigidbody>();
+                     targetRigidbody.isKinematic = false;
+              }
+       }
+}
