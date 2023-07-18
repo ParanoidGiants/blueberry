0a0c09cc65b70695176d658eaf3f315144df7724
locojoetive <johannes.mic@gmail.com>
2023-07-18 10:49:53 +0200

    fix: deliver fertilizer also after first visit

--- Assets/_Game/Entities/Level/End/EndLevel.cs
+++ Assets/_Game/Entities/Level/End/EndLevel.cs
@@ -11,6 +11,8 @@ namespace Level
         private FertilizerManager _manager;
         private Animator _animator;
 
+        private bool _isDelivering = false;
+
         private void Awake()
         {
             _endDirector = FindObjectOfType<GameCamera.CameraController>().EndDirector;
@@ -21,9 +23,13 @@ namespace Level
 
         private IEnumerator OnTriggerEnter(Collider other)
         {
-            if (_manager.IsAllDelivered || _manager.IsDelivering) yield break;
+            if (_isDelivering || _manager.IsAllDelivered) yield break;
+            _isDelivering = true;
             
-            yield return StartCoroutine(_manager.DeliverFertilizer());
+            yield return _manager.DeliverFertilizer();
+            
+            _isDelivering = false;
+            if (!_manager.IsAllDelivered) yield break;
             
             _endDirector.Play();
             _animator.enabled = true;
