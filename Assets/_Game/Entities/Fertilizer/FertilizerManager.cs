0d38b25a67870e9db95eaaeb16b6a913013b23d0
locojoetive <johannes.mic@gmail.com>
2023-07-18 17:25:23 +0200

    feat: polish end camera ride and add hint at the end of the game

--- Assets/_Game/Entities/Fertilizer/FertilizerManager.cs
+++ Assets/_Game/Entities/Fertilizer/FertilizerManager.cs
@@ -1,22 +1,26 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using DG.Tweening;
 
 namespace CollectableFetrilizer
 {
     public class FertilizerManager : MonoBehaviour
     {
+        [Header("Reference")]
+        public Creeper.MeshGenerator _meshGenerator;
+        
         private GameUI.FertilizerText _fertilizerText;
         private Transform _destination;
         private Fertilizer[] _fertilizers;
-        
+
+        private int _startAmount;
         private int _totalAmount;
+        private int _collectedAmount;
         private int _count;
-        [SerializeField] private bool _isDelivering;
+        
+        private bool _isDelivering;
         public bool IsDelivering => _isDelivering;
-        [SerializeField] private bool _isAllDelivered;
+        private bool _isAllDelivered;
         public bool IsAllDelivered => _isAllDelivered;
 
         private void Awake()
@@ -24,7 +28,7 @@ namespace CollectableFetrilizer
             _fertilizers = FindObjectsOfType<Fertilizer>();
             _fertilizerText = FindObjectOfType<GameUI.FertilizerText>();
             _destination = FindObjectOfType<Level.EndLevel>().transform;
-            _totalAmount = _fertilizers.Length;
+            _startAmount = _totalAmount = _fertilizers.Length;
         }
 
         private void Start()
@@ -35,6 +39,7 @@ namespace CollectableFetrilizer
         public void OnCollectFertilizer(Fertilizer fertilizer)
         {
             _count++;
+            _collectedAmount++;
             UpdateUI();
         }
 
@@ -43,9 +48,10 @@ namespace CollectableFetrilizer
             _isDelivering = true;
             
             var collectedToDeliver = _fertilizers.Where(x => x.IsCollected && !x.IsDelivered).ToList();
+            var startPosition = _meshGenerator.GetTipPosition();
             foreach (var deliveryItem in collectedToDeliver)
             {
-                StartCoroutine(deliveryItem.OnAnimateDelivery(_destination.position));
+                StartCoroutine(deliveryItem.OnAnimateDelivery(startPosition, _destination.position));
                 _count--;
                 _totalAmount--;
                 UpdateUI();
@@ -67,6 +73,11 @@ namespace CollectableFetrilizer
         public bool HasNewFertilizer()
         {
             return !_isDelivering && _fertilizers.Any(x => x.IsCollected && !x.IsDelivered);
+        }
+
+        public float GetFertilizerPercentage()
+        {
+            return (float) _collectedAmount / _startAmount;
         }
     }
 }
