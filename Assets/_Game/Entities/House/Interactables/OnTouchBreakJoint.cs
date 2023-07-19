f6132d9fc629710e53d4b585ca7f0656ab4c7630
locojoetive <johannes.mic@gmail.com>
2023-07-19 21:33:34 +0200

    refactor: add to-dos for playing sfx

--- Assets/_Game/Entities/House/Interactables/OnTouchBreakJoint.cs
+++ Assets/_Game/Entities/House/Interactables/OnTouchBreakJoint.cs
@@ -4,10 +4,12 @@ namespace Interactables
 {
     public class OnTouchBreakJoint : MonoBehaviour
     {
+        public string sfxId;
         private void OnCollisionEnter(Collision other)
         {
             if (!Utils.Helper.IsLayerPlayerPhysicsCollider(other.gameObject.layer)) return;
             
+            // TODO: Play sfx here and use sfxId
             var springJoint = GetComponent<SpringJoint>();
             springJoint.breakForce = 0;
             Destroy(this);
